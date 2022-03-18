a4dd8178d8294b4f67b1e0b56a7f2b9a7a72e8a3
Crack67 <martinuribeord13@gmail.com>
2022-03-17 18:17:28 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthomeapp/Config.java
@@ -0,0 +1,8 @@
+package mx.tecnm.cdhidalgo.smarthomeapp;
+
+public class Config {
+    public static final String URL ="http://172.17.123.105/smarthome/";
+    //192.168.52.1
+    //http://172.17.123.57/smarthome/
+
+}
